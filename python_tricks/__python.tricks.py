86023bc5c54c0c7b03dffbcc7b12a0bfee9d0d06
2junechung <2junechung@gmail.com>
2022-05-28 15:52:13 -0700

    new commit

--- python_tricks/__python.tricks.py
+++ python_tricks/__python.tricks.py
@@ -5,10 +5,12 @@ print(new_s)
 # string reverse
 new_s = s[::-1]
 new_s = s[0:4]
+last_element = s[-1]
 print(new_s)
 # palindrome checking 
 s = "abcdcba"
 print(all(s[i] == s[len(s)-i-1] for i in range(len(s))) )
+s == s[::-1]
 # dictionary 
 sampledict = { "key1": "value1", "key2": "value2"}
 print(sampledict["key1"])
@@ -20,10 +22,19 @@ for i in s:
     print("haha", i)
 # pop in list 
 s_array = [ 1, 2, 3, 4, 5, 6,7]
-s_array.pop(2)
+s_array.pop(2) # index 
+s_array.pop() # last element pop 
 s_array.remove(6)
 print(s_array)
 # sentence to list 
 test_string1 = "hello my name is June"
 print(test_string1.split())
 # change string to list 
+
+# replace in string 
+string.replace(",", " ")
+
+# sorting arrays 
+sorted(list) # this is only temporary when showing / actual result is same 
+list.sort() # changes the actual list / and returns nothing 
+
