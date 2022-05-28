662f5d6e558dee0394a0d017d67b2c2e42dac0f7
2junechung <2junechung@gmail.com>
2022-05-28 16:01:49 -0700

    change of tricks

--- python_tricks/__python.tricks.py
+++ python_tricks/__python.tricks.py
@@ -1,40 +1,56 @@
+
+######## STRING MANIPULATION ########
 # replace string
 s = "abcde"
 new_s = s.replace("c","A")
-print(new_s)
-# string reverse
-new_s = s[::-1]
-new_s = s[0:4]
-last_element = s[-1]
-print(new_s)
+
+# reverse string
+new_s = s[::-1] # reverse 
+new_s = s[0:4] # 0 - 3 index
+first_element = s[0]
+last_element = s[-1] # last character
+
 # palindrome checking 
 s = "abcdcba"
 print(all(s[i] == s[len(s)-i-1] for i in range(len(s))) )
 s == s[::-1]
-# dictionary 
-sampledict = { "key1": "value1", "key2": "value2"}
-print(sampledict["key1"])
-print(len(sampledict))
+
+# sentence to list 
+test_string1 = "hello my name is June"
+print(test_string1.split(" "))
+
+# replace in string 
+string.replace(",", " ")
 
 # for i in s 
 s = "abcdefg"
 for i in s:
     print("haha", i)
+
+#########################################
+######## LIST/ARRAY MANIPULATION ########
+
 # pop in list 
 s_array = [ 1, 2, 3, 4, 5, 6,7]
 s_array.pop(2) # index 
 s_array.pop() # last element pop 
 s_array.remove(6)
 print(s_array)
-# sentence to list 
-test_string1 = "hello my name is June"
-print(test_string1.split())
-# change string to list 
-
-# replace in string 
-string.replace(",", " ")
 
 # sorting arrays 
 sorted(list) # this is only temporary when showing / actual result is same 
 list.sort() # changes the actual list / and returns nothing 
+
+
+#########################################
+######## DICTIONARY ########
+# dictionary basic usage 
+sampledict = { "key1": "value1", "key2": "value2"}
+print(sampledict["key1"])
+print(len(sampledict))
+
+#########################################
+
+
+
 
---
+++ python_tricks/__test.tricks.py
@@ -0,0 +1,18 @@
+teststring = "abcdefghijk"
+test_string1 = "hello my name is June"
+test_string2 = "hello, hello, this, is, june"
+
+
+testlist = ["a", "b", "c", "d", "e", "f"]
+test_list1 = []
+test_list2 = []
+
+test_list1 = test_string1.split(" ")
+test_list2 = test_string2.split(", ")
+test_list3 = teststring.split("")
+
+
+print(teststring[-1])
+print(test_list1)
+print(test_list2)
+print(test_list3)
